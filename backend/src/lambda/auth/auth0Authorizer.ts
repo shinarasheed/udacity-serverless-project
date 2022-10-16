eed8265342acbcb06d6fa04537cc2e09204f31f0
shinarasheed <rasheedshinaopeyemi@gmail.com>
2022-10-16 15:12:34 +0100

    first commit

---
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -0,0 +1,86 @@
+import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import 'source-map-support/register'
+
+import Axios from 'axios'
+import { verify, decode } from 'jsonwebtoken'
+import { createLogger } from '../../utils/logger'
+import { Jwt } from '../../auth/Jwt'
+import { JwtPayload } from '../../auth/JwtPayload'
+
+const logger = createLogger('auth')
+
+const jwksUrl = 'https://dev-bfjkwot3.eu.auth0.com/.well-known/jwks.json'
+
+export const handler = async (
+  event: CustomAuthorizerEvent
+): Promise<CustomAuthorizerResult> => {
+  logger.info('Authorizing a user', event.authorizationToken)
+  try {
+    const jwtToken = await verifyToken(event.authorizationToken)
+    logger.info('User was authorized', jwtToken)
+
+    return {
+      principalId: jwtToken.sub,
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  } catch (e) {
+    logger.error('User not authorized', { error: e.message })
+
+    return {
+      principalId: 'user',
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Deny',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  }
+}
+
+async function verifyToken(authHeader: string): Promise<JwtPayload> {
+  const token = getToken(authHeader)
+  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  const kid = jwt.header.kid
+  let cert
+
+  try {
+    const jwks = await Axios.get(jwksUrl)
+    const signingKey = jwks.data.keys.find((k) => k.kid === kid)
+
+    if (!signingKey) {
+      throw new Error(`No signing key matching the kid '${kid}' was found`)
+    }
+
+    cert = `-----BEGIN CERTIFICATE-----\n${signingKey.x5c[0]}\n-----END CERTIFICATE-----`
+  } catch (e) {
+    logger.error('Failed to retrieve auth0 certificate', { error: e.message })
+  }
+
+  return verify(token, cert, { algorithms: ['RS256'] }) as JwtPayload
+}
+
+function getToken(authHeader: string): string {
+  if (!authHeader) throw new Error('No authentication header')
+
+  if (!authHeader.toLowerCase().startsWith('bearer '))
+    throw new Error('Invalid authentication header')
+
+  const split = authHeader.split(' ')
+  const token = split[1]
+
+  return token
+}
