ce726b7bc4ee86b60201ded3e3554b3600f36f39
shinarasheed <rasheedshinaopeyemi@gmail.com>
2022-10-20 11:16:32 +0100

    intial commit

---
+++ src/lambda/http/updateTodo.ts
@@ -0,0 +1,31 @@
+import 'source-map-support/register'
+
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyHandler,
+  APIGatewayProxyResult
+} from 'aws-lambda'
+import { updateTodoItem } from '../../helpers/todos'
+import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
+import { getUserId } from '../utils'
+
+export const handler: APIGatewayProxyHandler = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  console.log('Processing event: ', event)
+
+  const userId = getUserId(event)
+  const todoId = event.pathParameters.todoId
+  const updatedTodoRequest: UpdateTodoRequest = JSON.parse(event.body)
+
+  await updateTodoItem(updatedTodoRequest, userId, todoId)
+
+  return {
+    statusCode: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({})
+  }
+}
